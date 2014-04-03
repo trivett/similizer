f04d1dbca6539efa54e7a989772648a76c3e365d
trivett <trivett@gmail.com>
2014-04-03 12:46:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Similizer::Application.config.secret_key_base = '0215e1d4df6feda4993e0b8511e8d4cba94c1de5e090ea58264fa60a00554f862927375c049d7f96de4aa52836ae4569f612763e591cfb8b831c8677eb1be682'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Similizer::Application.config.session_store :cookie_store, key: '_similizer_session'
