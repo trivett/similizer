85e09f5db6221b54fad7a462860b8a60ab3e972c
trivett <trivett@gmail.com>
2014-04-03 12:59:10 -0400

    added gem, key initializer, and welcome contorller

---
+++ config/initializers/echowrap.rb
@@ -0,0 +1,5 @@
+Echowrap.configure do |config|
+  config.api_key =       'JGIG5ZHBEQH9OST2P'
+  config.consumer_key =  'bbf131dc02f75f66351b2bc98f351dde'
+  config.shared_secret = 'PnGoAR+xSbK6XnFwIeRL+w'
+end
